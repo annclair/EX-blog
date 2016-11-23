b1dcaae64254d6808a9aaaf7639c2d37e28b982c
apprenti <anneclaire.sebirou@gmail.com>
2016-11-23 10:34:28 +0100

    debut services bd ... non fonctionnel

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+((app) => {
+
+//   app.service ('blogsService', function ($http){
+//     return {
+//       get() {
+//         return $http.get('/posts.json')
+//       }
+//     }
+//   }
+// )
+
+})(angular.module('app', [
+  'app.services',
+  'app.blogs'
+]))
